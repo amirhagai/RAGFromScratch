5467853cd8fbada916f616416ddf2af6eb52b98f
amirhagai <amir.hagai@mail.huji.ac.il>
2024-12-24 11:54:31 +0200

    start db managment

--- RAG_start.py
+++ RAG_start.py
@@ -10,6 +10,7 @@ torch.use_deterministic_algorithms(True, warn_only=True)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
+import re
 import bs4
 from langchain import hub
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -27,8 +28,9 @@ from operator import itemgetter
 import transformers
 from get_llama import get_llama
 from get_embedding_model import EmbedModel
-
-
+from chromadb import Client
+from chromadb.config import Settings
+import chromadb
 
 
 def load_web_doc(web_addresses="https://lilianweng.github.io/posts/2023-06-23-agent/"):
@@ -48,10 +50,21 @@ def split_docs(docs, chunk_size=500, chunk_overlap=300):
     splits = text_splitter.split_documents(docs)
     return text_splitter, splits
 
-def embed(splits, k=5):
+def embed(splits, k=5, delete_old=False, collection_name="example_collection"):
+
+    if delete_old:
+        client_settings = Settings(persist_directory="./chroma_langchain_db")
+        client = chromadb.PersistentClient(path="./chroma_langchain_db", settings=client_settings)
+
+        # Check if the collection exists
+        existing_collections = [col.name for col in client.list_collections()]
+        if collection_name in existing_collections:
+            if delete_old:
+                # Delete the existing collection
+                client.delete_collection(collection_name)
     vectorstore = Chroma.from_documents(documents=splits, 
                                         embedding=EmbedModel(), #OpenAIEmbeddings(),
-                                        collection_name="example_collection",
+                                        collection_name=collection_name,
                                         persist_directory="./chroma_langchain_db",)
     retriever = vectorstore.as_retriever(search_kwargs={"k": k})
     return vectorstore, retriever
@@ -92,23 +105,20 @@ def get_chat_prompt(custom_instruction="Answer the question based only on the fo
     return prompt
 
 
-def get_multy_queries_generator(str_num_queries):
+def get_multy_queries_generator(str_num_queries, llm, question="What is Task Decomposition?"):
     # Multi Query: Different Perspectives
     template = f"""You are an AI language model assistant. Your task is to generate {str_num_queries}
     different versions of the given user question to retrieve relevant documents from a vector 
-    database. By generating multiple perspectives on the user question, your goal is to help
+    database. please number the questions (e.g 1., 2., ...) By generating multiple perspectives on the user question, your goal is to help
     the user overcome some of the limitations of the distance-based similarity search. 
+    please only provide the questions and nothing else
     Provide these alternative questions separated by newlines. Original question: {{question}}"""
     prompt_perspectives = ChatPromptTemplate.from_template(template)
     # llm = ChatOpenAI(temperature=0)
-    llm = get_llama()
-    generate_queries = (
-        prompt_perspectives 
-        | llm
-        | StrOutputParser() 
-        | (lambda x: x.split("\n"))
-    )
-    return generate_queries
+    res = llm(prompt_perspectives.invoke(question).messages[0].content, pipeline_kwargs={'return_full_text' : False})
+    result = re.sub(r'^\s*$\n', '', res, flags=re.MULTILINE)
+    simmilar_queries = (lambda x: x.split("\n"))(result)
+    return lambda x : simmilar_queries
 
 
 def get_unique_union(documents: list[list]):
@@ -120,28 +130,26 @@ def get_unique_union(documents: list[list]):
     # Return
     return [loads(doc) for doc in unique_docs]
 
-def use_pipe_directly_example(msg):
+def use_pipe_directly_example(llm, msg):
     return llm.pipeline(
                     [msg],
                     **{'return_tensors' : True},
                 )
 
-def get_multy_query_rag_retrival_chain(retriever):
+def get_multy_query_rag_retrival_chain(retriever, llm, how_many_questions, question):
     # Retrieve
-    generate_queries =  get_multy_queries_generator("five")
+    generate_queries =  get_multy_queries_generator(how_many_questions, llm, question)
     retrieval_chain = generate_queries | retriever.map() | get_unique_union
-    return retrieval_chain, generate_queries 
+    return retrieval_chain, generate_queries
 
-def multy_query_rag(retriever, prompt, llm, question):
-    retrieval_chain, generate_queries  = get_multy_query_rag_retrival_chain(retriever)
-    rag_chain = (
+def multy_query_rag(retriever, prompt, llm, question, how_many_questions="five"):
+    retrieval_chain, generate_queries  = get_multy_query_rag_retrival_chain(retriever, llm, how_many_questions, question)
+    retrive_prompt_chain = (
         {"context": retrieval_chain, "question": itemgetter("question")} 
         | prompt
-        | llm
-        | StrOutputParser()
     )
-    result = rag_chain.invoke({"question":question})
-    return result
+    retrive_prompt = retrive_prompt_chain.invoke({"question":question})
+    return retrieval_chain, generate_queries, retrive_prompt_chain, retrive_prompt 
 
 
 def get_retrive_prompt_chain(retriever, prompt):
@@ -156,14 +164,25 @@ def llama_rag(return_full_text):
     docs = load_web_doc()
     text_splitter, splits = split_docs(docs=docs)
     vectorstore, retriever = embed(splits=splits)
-    prompt, llm = load_prompt_and_model(max_new_tokns=256)
+    prompt, llm = load_prompt_and_model()
     retrive_prompt_chain = get_retrive_prompt_chain(retriever, prompt)
     retrive_prompt = retrive_prompt_chain.invoke("What is Task Decomposition?")
     respones = llm.invoke(retrive_prompt.messages[0].content, pipeline_kwargs={'return_full_text' : return_full_text})
     return docs, text_splitter, splits, vectorstore, retriever, prompt, llm, retrive_prompt_chain, retrive_prompt, respones
 
+def llama_multy_query_rag():
+    docs = load_web_doc()
+    text_splitter, splits = split_docs(docs=docs)
+    vectorstore, retriever = embed(splits=splits, delete_old=True)
+    prompt, llm = load_prompt_and_model(max_new_tokns=512)
+    retrieval_chain, generate_queries, retrive_prompt_chain, retrive_prompt = multy_query_rag(retriever, prompt, llm, question="What is Task Decomposition?", how_many_questions="five")
 
+    print("############################################", end="\n\n\n")
+    result = llm(llm.invoke(retrive_prompt.messages[0].content, pipeline_kwargs={'return_full_text' : False}))
+    print(result)
 
 if __name__ == '__main__':
-    docs, text_splitter, splits, vectorstore, retriever, prompt, llm, retrive_prompt_chain, retrive_prompt, respones = llama_rag(return_full_text=False)
-    print(respones)
+
+    llama_multy_query_rag()
+    # docs, text_splitter, splits, vectorstore, retriever, prompt, llm, retrive_prompt_chain, retrive_prompt, respones = llama_rag(return_full_text=False)
+    # print(respones)
